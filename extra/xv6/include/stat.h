183dafa9f2b980feeafcb9545a174f8ecf3957cd
Xi Song <xi@adelie-03.cs.wisc.edu>
2015-03-30 19:52:54 -0500

    new file:   extra/xv6/.gitignore 	new file:   extra/xv6/FILES 	new file:   extra/xv6/Makefile 	new file:   extra/xv6/Makefile~ 	new file:   extra/xv6/README 	new file:   extra/xv6/include/fcntl.h 	new file:   extra/xv6/include/fs.h 	new file:   extra/xv6/include/param.h 	new file:   extra/xv6/include/stat.h 	new file:   extra/xv6/include/syscall.h 	new file:   extra/xv6/include/traps.h 	new file:   extra/xv6/include/types.h 	new file:   extra/xv6/include/x86.h 	new file:   extra/xv6/kernel/asm.h 	new file:   extra/xv6/kernel/bio.c 	new file:   extra/xv6/kernel/bootasm.S 	new file:   extra/xv6/kernel/bootmain.c 	new file:   extra/xv6/kernel/bootother.S 	new file:   extra/xv6/kernel/buf.h 	new file:   extra/xv6/kernel/console.c 	new file:   extra/xv6/kernel/data.S 	new file:   extra/xv6/kernel/defs.h 	new file:   extra/xv6/kernel/elf.h 	new file:   extra/xv6/kernel/exec.c 	new file:   extra/xv6/kernel/file.c 	new file:   extra/xv6/kernel/file.h 	new file:   extra/xv6/kernel/fs.c 	new file:   extra/xv6/kernel/ide.c 	new file:   extra/xv6/kernel/initcode.S 	new file:   extra/xv6/kernel/ioapic.c 	new file:   extra/xv6/kernel/kalloc.c 	new file:   extra/xv6/kernel/kbd.c 	new file:   extra/xv6/kernel/kbd.h 	new file:   extra/xv6/kernel/lapic.c 	new file:   extra/xv6/kernel/main.c 	new file:   extra/xv6/kernel/makefile.mk 	new file:   extra/xv6/kernel/mmu.h 	new file:   extra/xv6/kernel/mp.c 	new file:   extra/xv6/kernel/mp.h 	new file:   extra/xv6/kernel/multiboot.S 	new file:   extra/xv6/kernel/picirq.c 	new file:   extra/xv6/kernel/pipe.c 	new file:   extra/xv6/kernel/proc.c 	new file:   extra/xv6/kernel/proc.h 	new file:   extra/xv6/kernel/sign.pl 	new file:   extra/xv6/kernel/spinlock.c 	new file:   extra/xv6/kernel/spinlock.h 	new file:   extra/xv6/kernel/string.c 	new file:   extra/xv6/kernel/swtch.S 	new file:   extra/xv6/kernel/syscall.c 	new file:   extra/xv6/kernel/sysfile.c 	new file:   extra/xv6/kernel/sysfunc.h 	new file:   extra/xv6/kernel/sysproc.c 	new file:   extra/xv6/kernel/timer.c 	new file:   extra/xv6/kernel/trap.c 	new file:   extra/xv6/kernel/trapasm.S 	new file:   extra/xv6/kernel/uart.c 	new file:   extra/xv6/kernel/vectors.pl 	new file:   extra/xv6/kernel/vm.c 	new file:   extra/xv6/tools/dot-bochsrc 	new file:   extra/xv6/tools/dot-gdbinit 	new file:   extra/xv6/tools/gdbutil 	new file:   extra/xv6/tools/makefile.mk 	new file:   extra/xv6/tools/mkfs.c 	new file:   extra/xv6/user/cat.c 	new file:   extra/xv6/user/echo.c 	new file:   extra/xv6/user/forktest.c 	new file:   extra/xv6/user/grep.c 	new file:   extra/xv6/user/init.c 	new file:   extra/xv6/user/kill.c 	new file:   extra/xv6/user/ln.c 	new file:   extra/xv6/user/ls.c 	new file:   extra/xv6/user/makefile.mk 	new file:   extra/xv6/user/mkdir.c 	new file:   extra/xv6/user/printf.c 	new file:   extra/xv6/user/rm.c 	new file:   extra/xv6/user/sh.c 	new file:   extra/xv6/user/stressfs.c 	new file:   extra/xv6/user/tester.c 	new file:   extra/xv6/user/ulib.c 	new file:   extra/xv6/user/umalloc.c 	new file:   extra/xv6/user/user.h 	new file:   extra/xv6/user/usertests.c 	new file:   extra/xv6/user/usys.S 	new file:   extra/xv6/user/wc.c 	new file:   extra/xv6/user/zombie.c
    	deleted:    p3/xv6-mem/xv6-passDONE!/kernel/2

---
+++ extra/xv6/include/stat.h
@@ -0,0 +1,21 @@
+#ifndef _STAT_H_
+#define _STAT_H_
+
+// Stat struct definition, for use with stat syscall
+
+#define T_DIR  1   // Directory
+#define T_FILE 2   // File
+#define T_DEV  3   // Special device
+#define T_MIRRORED 4
+
+struct stat {
+  short type;  // Type of file
+  int dev;     // Device number
+  uint ino;    // Inode number on device
+  short nlink; // Number of links to file
+  //uint size;   // Size of file in bytes
+  uint logical_size;   // Size of file in bytes
+  uint physical_size;   // Size of file in bytes
+};
+
+#endif // _STAT_H_
